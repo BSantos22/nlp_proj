da2de979c7dadd66682b13c0a639f0e43ad1a2c5
David Rosenbusch <david.rosenbusch@googlemail.com>
2018-10-10 11:16:07 +0100

    whoops

--- main/getOldTweets.py
+++ main/getOldTweets.py
@@ -1,32 +1,102 @@
 import sys
 import json
+
 if sys.version_info[0] < 3:
-    import got
+	import got
 else:
-    import got3 as got
+	import got3 as got
 import os
+import nltk
+from nltk.corpus import stopwords
+import operator
+from nltk.tokenize import TweetTokenizer
+import datetime
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
 
-outputDir = "search_results"
+nltk.download('stopwords')
 
-def main(topic):
-	def printTweet(t):
-		return {'Retweets': t.retweets,
-				'Text': t.text,
-				'Mentions': t.mentions,
-				'Hashtags': t.hashtags,
-				'Date': str(t.date)}
+tweets_dir = "results"
+raw_dir = tweets_dir + "/filteredOut"
+filtered_dir = tweets_dir + "/tweets"
+sent_dir = tweets_dir + "/sentiments"
+time_format = "%Y-%m-%d"
 
-	# Example 2 - Get tweets by query search
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch(topic).setSince('2015-05-01').setUntil('2015-09-30').setMaxTweets(100)
-	fileName = outputDir + '/got_' + topic + '.txt'
-	open(fileName, 'w+')
-	for x in range(100):
-		tweet = printTweet(got.manager.TweetManager.getTweets(tweetCriteria)[x])
-		print(json.dumps(tweet, sort_keys=True, indent=4), file=open(fileName, 'a'))
+def tweet_to_json(tweet):
+	return {'Text': tweet.text,
+			'Retweets': tweet.retweets,
+			'Favorites': tweet.favorites,
+			'Hashtags': tweet.hashtags,
+			'ID': tweet.id,
+			'Username': tweet.username,
+			'Permalink': tweet.permalink,
+			'Mentions': tweet.mentions,
+			'Date': str(tweet.date)}
+
+
+def tweet_is_english(tweet):
+	text = tweet['Text']
+	languages_ratios = {}
+	words = TweetTokenizer().tokenize(text)
+	words_lower = TweetTokenizer().tokenize(text.lower())
+	words_set = set(words_lower)
+	for language in stopwords.fileids():
+		stopwords_set = set(stopwords.words(language))
+		common_elements = words_set.intersection(stopwords_set)
+		languages_ratios[language] = len(common_elements)
+	predicted_language = max(languages_ratios.items(), key=operator.itemgetter(1))[0]
+	if predicted_language == "english":
+		return words
+	return None
+
+
+def main(topic, tweets_per_month, start, fame):
+	file_name = topic + "_" + str(tweets_per_month) + "_" + str(fame)
+
+	def tweet_is_fame_dayum(tweet):
+		return 2 * int(tweet["Retweets"]) + int(tweet['Favorites']) > fame
+
+	end = datetime.datetime.now()
+	num_tweets = 0
+	num_filtered = 0
+	tweets_file_name = raw_dir + '/' + file_name + '.txt'
+	tweets_file_name_en = filtered_dir + '/' + file_name + '.txt'
+	while start < end:
+		until = start + relativedelta(days=1)
+		print(start.strftime(time_format) + " to " + until.strftime(time_format))
+		tweet_criteria = got.manager.TweetCriteria().setQuerySearch(topic).setSince(start.strftime(time_format)).setUntil(
+			until.strftime(time_format)).setTopTweets(True).setMaxTweets(tweets_per_month)
+		tweets = got.manager.TweetManager.getTweets(tweet_criteria)
+		for x in range(len(tweets)):
+			tweet = tweet_to_json(tweets[x])
+			d = json.dumps(tweet, sort_keys=True, indent=4)
+			used = False
+			if tweet_is_fame_dayum(tweet):
+				words_set = tweet_is_english(tweet)
+				if words_set is not None:
+					used = True
+					num_filtered += 1
+					tweet["tokens"] = str(words_set)
+					d = json.dumps(tweet, sort_keys=True, indent=4)
+					print(d, file=open(tweets_file_name_en, 'a+'))
+			if not used:
+				print(d, file=open(tweets_file_name, 'a+'))
+		num_tweets += len(tweets)
+		print(str(num_tweets) + " tweets, " + str(num_filtered) + " filtered")
+		start = until
 
 
 if __name__ == '__main__':
-	if not os.path.exists(outputDir):
-		os.makedirs(outputDir)
-	main(input('topic: '))
-
+	if not os.path.exists(tweets_dir):
+		os.makedirs(tweets_dir)
+	if not os.path.exists(raw_dir):
+		os.makedirs(raw_dir)
+	if not os.path.exists(filtered_dir):
+		os.makedirs(filtered_dir)
+	if not os.path.exists(sent_dir):
+		os.makedirs(sent_dir)
+	tweet_topic = input('topic: ')
+	num_per_month = input('tweets per day: ')
+	start_date = input("start date: (yyyy-mm-dd): ")
+	min_fame = input("min fame (2 * retweets + favourites): ")
+	main(tweet_topic, int(num_per_month), parser.parse(start_date), int(min_fame))
